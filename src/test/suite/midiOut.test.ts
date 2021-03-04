63489be69f3916ce44a132c4c4372377222fe473
lhl2617 <lhlee888@hotmail.com>
2021-03-04 21:54:43 +0000

    Tests: reorg midiOut

--- src/test/suite/midiOut.test.ts
+++ src/test/suite/midiOut.test.ts
@@ -5,180 +5,190 @@ import { MIDIOut } from '../../midiOut';
 
 suite(`MIDIOut test suite`, () => {
 	vscode.window.showInformationMessage('Start MIDIOut tests.');
-    test(`msToMMSS - 0`, () => {
-        const ms = 0;
-        const got = MIDIOut.msToMMSS(ms);
-        const exp = `0:00`;
+
+    suite(`msToMMSS suite`, () => {
+        const msToMMSSTests: {
+            name: string;
+            ms: number;
+            want: string;
+        }[] = [
+            {
+                name: "0",
+                ms: 0, 
+                want: `0:00`,
+            },
+            {
+                name: "almost 1 sec",
+                ms: 999,
+                want: `0:01`,
+            },
+            {
+                name: "exactly 1 sec",
+                ms: 1000,
+                want: `0:01`,
+            },
+            {
+                name: "bit more than 1 sec",
+                ms: 1001,
+                want: `0:01`,
+            },
+            {
+                name: "1 min",
+                ms: 60000,
+                want: `1:00`,
+            },
+            {
+                name: "large number",
+                ms: 6023000,
+                want: `100:23`,
+            },
+        ];
+
+        const msToMMSSTestsException: {
+            name: string;
+            ms: number;
+            error: Error;
+        }[] = [
+            {
+                name: "Negative time",
+                ms: -1,
+                error: new Error(`Time cannot be negative`),
+            }
+        ];
+
+        for (const t of msToMMSSTests) {
+            const { name, ms, want } = t;
+            test(name, () => {
+                const got = MIDIOut.msToMMSS(ms);
+                assert.strictEqual(got, want);
+            });
+        }
         
-        assert.strictEqual(got, exp);
+        for (const t of msToMMSSTestsException) {
+            const { name, ms, error } = t;
+            test(name, () => {
+                const expectThrow = () => MIDIOut.msToMMSS(ms);
+                assert.throws(expectThrow, error);
+            });
+        }
     });
 
-    test(`msToMMSS - almost 1 sec`, () => {
-        const ms = 999;
-        const got = MIDIOut.msToMMSS(ms);
-        const exp = `0:01`;
+    suite(`MMSSToms suite`, () => {
+        const MMSSTomsTestsException: {
+            name: string;
+            mmss: string;
+            error: Error;
+        }[] = [
+            {
+                name: "Empty string",
+                mmss: ``,
+                error: new Error(` does not match the required syntax: \/\^[0-9]+:[0-5][0-9]\$\/. Valid examples: 1:23, 10:59, 0:12`),
+            },
+            {
+                name: "Number only",
+                mmss: `12`,
+                error: new Error(`12 does not match the required syntax: \/\^[0-9]+:[0-5][0-9]\$\/. Valid examples: 1:23, 10:59, 0:12`),
+            },
+            {
+                name: "Number only 2",
+                mmss: `11232`,
+                error: new Error(`11232 does not match the required syntax: \/\^[0-9]+:[0-5][0-9]\$\/. Valid examples: 1:23, 10:59, 0:12`),
+            },
+            {
+                name: "0:0",
+                mmss: `0:0`,
+                error: new Error(`0:0 does not match the required syntax: \/\^[0-9]+:[0-5][0-9]\$\/. Valid examples: 1:23, 10:59, 0:12`),
+            },
+            {
+                name: "0:60",
+                mmss: `0:60`,
+                error: new Error(`0:60 does not match the required syntax: \/\^[0-9]+:[0-5][0-9]\$\/. Valid examples: 1:23, 10:59, 0:12`),
+            },
+            {
+                name: "0:012",
+                mmss: `0:012`,
+                error: new Error(`0:012 does not match the required syntax: \/\^[0-9]+:[0-5][0-9]\$\/. Valid examples: 1:23, 10:59, 0:12`),
+            },
+        ];
+
+        const MMSSTomsTests: {
+            name: string;
+            mmss: string;
+            want: number;
+        }[] = [
+            {
+                name: "Valid 1",
+                mmss: `0:00`,
+                want: 0,
+            },
+            {
+                name: "Valid 2",
+                mmss: `0:59`,
+                want: 59000,
+            },
+            {
+                name: "Valid 3",
+                mmss: `1:00`,
+                want: 60000,
+            },
+            {
+                name: "Valid 4",
+                mmss: `123:34`,
+                want: 7414000,
+            },
+        ];
+
+        for (const t of MMSSTomsTests) {
+            const { name, mmss, want } = t;
+            test(name, () => {
+                const got = MIDIOut.MMSSToms(mmss);
+                assert.strictEqual(got, want);
+            });
+        }
         
-        assert.strictEqual(got, exp);
-    });
-    
-    test(`msToMMSS - exactly 1 sec`, () => {
-        const ms = 1000;
-        const got = MIDIOut.msToMMSS(ms);
-        const exp = `0:01`;
-        
-        assert.strictEqual(got, exp);
-    });
-    
-    test(`msToMMSS - bit more than 1 sec`, () => {
-        const ms = 1001;
-        const got = MIDIOut.msToMMSS(ms);
-        const exp = `0:01`;
-        
-        assert.strictEqual(got, exp);
+        for (const t of MMSSTomsTestsException) {
+            const { name, mmss, error } = t;
+            test(name, () => {
+                const expectThrow = () => MIDIOut.MMSSToms(mmss);
+                assert.throws(expectThrow, error);
+            });
+        }
     });
 
-    test(`msToMMSS - 1 min`, () => {
-        const ms = 60000;
-        const got = MIDIOut.msToMMSS(ms);
-        const exp = `1:00`;
-        
-        assert.strictEqual(got, exp);
-    });
-    
-    test(`msToMMSS - large number`, () => {
-        const ms = 6023000;
-        const got = MIDIOut.msToMMSS(ms);
-        const exp = `100:23`;
-        
-        assert.strictEqual(got, exp);
-    });
-    
-    test(`msToMMSS - negative time exception`, () => {
-        const ms = -1;
-        const expectThrow = () => MIDIOut.msToMMSS(ms);
-        
-        assert.throws(expectThrow, new Error(`Time cannot be negative`));
-    });
+    suite(`validateMIDIStartTimeInput suite`, () => {
+        const tests: {
+            name: string;
+            durationMS: number;
+            mmss: string;
+            want: string | undefined;
+        }[] = [
+            {
+                name: "Longer",
+                durationMS: 1999,
+                mmss: '0:02',
+                want: `Duration 0:02 is longer than duration of actual MIDI file 0:02`,
+            },
+            {
+                name: "OK 1",
+                durationMS: 2000,
+                mmss: '0:02',
+                want: undefined,
+            },
+            {
+                name: "OK 2",
+                durationMS: 2001,
+                mmss: '0:02',
+                want: undefined,
+            },
+        ];
 
-    test(`MMSSToms - exception empty string`, () => {
-        const mmss = ``;
-        const expectThrow = () => MIDIOut.MMSSToms(mmss);
-        
-        assert.throws(expectThrow,new Error(`${mmss} does not match the required syntax: \/\^[0-9]+:[0-5][0-9]\$\/. Valid examples: 1:23, 10:59, 0:12`));
-    });
-    
-    test(`MMSSToms - exception number`, () => {
-        const mmss = `12`;
-        const expectThrow = () => MIDIOut.MMSSToms(mmss);
-        
-        assert.throws(expectThrow,new Error(`${mmss} does not match the required syntax: \/\^[0-9]+:[0-5][0-9]\$\/. Valid examples: 1:23, 10:59, 0:12`));
-    });
-    
-    test(`MMSSToms - exception number 2`, () => {
-        const mmss = `11232`;
-        const expectThrow = () => MIDIOut.MMSSToms(mmss);
-        
-        assert.throws(expectThrow,new Error(`${mmss} does not match the required syntax: \/\^[0-9]+:[0-5][0-9]\$\/. Valid examples: 1:23, 10:59, 0:12`));
-    });
-    
-    test(`MMSSToms - exception 0:0`, () => {
-        const mmss = `0:0`;
-        const expectThrow = () => MIDIOut.MMSSToms(mmss);
-        
-        assert.throws(expectThrow,new Error(`${mmss} does not match the required syntax: \/\^[0-9]+:[0-5][0-9]\$\/. Valid examples: 1:23, 10:59, 0:12`));
-    });
-
-    test(`MMSSToms - exception 0:60`, () => {
-        const mmss = `0:60`;
-        const expectThrow = () => MIDIOut.MMSSToms(mmss);
-        
-        assert.throws(expectThrow,new Error(`${mmss} does not match the required syntax: \/\^[0-9]+:[0-5][0-9]\$\/. Valid examples: 1:23, 10:59, 0:12`));
-    });
-
-    test(`MMSSToms - exception 0:000`, () => {
-        const mmss = `0:000`;
-        const expectThrow = () => MIDIOut.MMSSToms(mmss);
-        
-        assert.throws(expectThrow,new Error(`${mmss} does not match the required syntax: \/\^[0-9]+:[0-5][0-9]\$\/. Valid examples: 1:23, 10:59, 0:12`));
-    });
-
-    test(`MMSSToms - exception 0:012`, () => {
-        const mmss = `0:012`;
-        const expectThrow = () => MIDIOut.MMSSToms(mmss);
-        
-        assert.throws(expectThrow,new Error(`${mmss} does not match the required syntax: \/\^[0-9]+:[0-5][0-9]\$\/. Valid examples: 1:23, 10:59, 0:12`));
-    });
-
-    test(`MMSSToms - exception 0:012`, () => {
-        const mmss = `0:012`;
-        const expectThrow = () => MIDIOut.MMSSToms(mmss);
-        
-        assert.throws(expectThrow,new Error(`${mmss} does not match the required syntax: \/\^[0-9]+:[0-5][0-9]\$\/. Valid examples: 1:23, 10:59, 0:12`));
-    });
-
-    test(`MMSSToms - valid 1`, () => {
-        const mmss = `0:00`;
-        const got = MIDIOut.MMSSToms(mmss);
-        const exp = 0;
-
-        assert.strictEqual(got, exp);
-    });
-
-    test(`MMSSToms - valid 2`, () => {
-        const mmss = `0:59`;
-        const got = MIDIOut.MMSSToms(mmss);
-        const exp = 59000;
-
-        assert.strictEqual(got, exp);
-    });
-    
-    test(`MMSSToms - valid 3`, () => {
-        const mmss = `1:00`;
-        const got = MIDIOut.MMSSToms(mmss);
-        const exp = 60000;
-
-        assert.strictEqual(got, exp);
-    });
-    
-    test(`MMSSToms - valid 4`, () => {
-        const mmss = `123:34`;
-        const got = MIDIOut.MMSSToms(mmss);
-        const exp = 7414000;
-
-        assert.strictEqual(got, exp);
-    });
-
-    test(`validateMIDIStartTimeInput - exception longer`, () => {
-        const durationMS = 1999;
-        const mmss = '0:02';
-        const durationMMSS = MIDIOut.msToMMSS(durationMS);
-
-        const got = MIDIOut.validateMIDIStartTimeInput(durationMS, durationMMSS, mmss);
-        const exp = `Duration ${mmss} is longer than duration of actual MIDI file ${durationMMSS}`;
-
-        assert.strictEqual(got, exp);
-    });
-    
-
-    test(`validateMIDIStartTimeInput - valid 1`, () => {
-        const durationMS = 2000;
-        const mmss = '0:02';
-        const durationMMSS = MIDIOut.msToMMSS(durationMS);
-
-        const got = MIDIOut.validateMIDIStartTimeInput(durationMS, durationMMSS, mmss);
-        const exp = undefined;
-
-        assert.strictEqual(got, exp);
-    });
-
-    test(`validateMIDIStartTimeInput - valid 2`, () => {
-        const durationMS = 2001;
-        const mmss = '0:02';
-        const durationMMSS = MIDIOut.msToMMSS(durationMS);
-
-        const got = MIDIOut.validateMIDIStartTimeInput(durationMS, durationMMSS, mmss);
-        const exp = undefined;
-
-        assert.strictEqual(got, exp);
+        for (const t of tests) {
+            const { name, durationMS, mmss, want } = t;
+            test(name, () => {
+                const durationMMSS = MIDIOut.msToMMSS(durationMS);
+                const got = MIDIOut.validateMIDIStartTimeInput(durationMS, durationMMSS, mmss);
+                assert.strictEqual(got, want);
+            });
+        }
     });
 });

fc642adbc4cc5038eca17f83ccfe7c3942c991d0
lhl2617 <lhlee888@hotmail.com>
2021-03-04 19:54:31 +0000

    Tests: Fix typo in debug suite test

--- src/test/suite/debug.test.ts
+++ src/test/suite/debug.test.ts
@@ -6,7 +6,7 @@ import { debugMode } from '../../consts';
 /// make sure debugMode is false when deployed
 
 suite(`Debug Test Suite`, () => {
-    vscode.window.showInformationMessage('Start intellisense tests.');
+    vscode.window.showInformationMessage('Start debug tests.');
     test(`debug - debugMode false`, () => {
         assert.equal(debugMode, false);
     });
